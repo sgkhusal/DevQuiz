d4affbd02ec9c0691f7d1441f38feac97cb1e38d
Sheela Guilhermino Khusal <sheela.khusal.08@gmail.com>
2021-04-24 20:38:42 -0300

    Added home repository - lesson 03

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,7 +1,5 @@
-import 'package:devquiz/core/app_images.dart';
+import 'package:devquiz/home/home_repository.dart';
 import 'package:devquiz/home/home_state.dart';
-import 'package:devquiz/shared/models/answer_model.dart';
-import 'package:devquiz/shared/models/question_model.dart';
 import 'package:devquiz/shared/models/quiz_model.dart';
 import 'package:devquiz/shared/models/user_model.dart';
 import 'package:flutter/foundation.dart';
@@ -17,49 +15,17 @@ class HomeController {
   UserModel? user; // a ? inicia o user como null
   List<QuizModel>? quizzes;
 
+  final repository = HomeRepository();
+
   void getUser() async{
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-    user = UserModel(
-      name: "Sheela",
-      photoUrl: "https://www.bonsfluidos.com.br/images/large/2020/07/06/o-fotografo-shaaz-jung-fez-fotos-incriveis-do-leopardo-negro-1102024.jpg",
-    );
+    user = await repository.getUser();
     state = HomeState.success;
   }
 
   void getQuizes() async{
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-    quizzes = [
-      QuizModel(
-        imagem: AppImages.blocks,
-        title: "NLW 5 Flutter",
-        level: Level.facil,
-        questionsAnswered: 1,
-        questions: [
-          QuestionModel(
-            title: "Você está curtindo o Flutter?",
-            answers: [
-              AnswerModel(title: "Estou curtindo"),
-              AnswerModel(title: "Estou amando"),
-              AnswerModel(title: "Muito top"),
-              AnswerModel(title: "Show de bola",
-              isRight: true),
-            ],
-          ),
-          QuestionModel(
-            title: "Você está curtindo o Flutter?",
-            answers: [
-              AnswerModel(title: "Estou curtindo"),
-              AnswerModel(title: "Estou amando"),
-              AnswerModel(title: "Muito top"),
-              AnswerModel(title: "Show de bola",
-              isRight: true),
-            ],
-          )
-        ]
-      )
-    ];
+    quizzes = await repository.getQuizzes();
     state = HomeState.success;
   }
 }
