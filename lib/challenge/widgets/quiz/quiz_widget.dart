d68d115acbdfd1f866f87b3710db26d08053083e
Sheela Guilhermino Khusal <sheela.khusal.08@gmail.com>
2021-04-25 17:34:15 -0300

    Added quiz controller - lesson 04

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,11 +1,21 @@
 import 'package:devquiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/shared/models/answer_model.dart';
 import 'package:devquiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
   const QuizWidget({Key? key, required this.question}) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answer(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
@@ -14,24 +24,19 @@ class QuizWidget extends StatelessWidget {
         children: [
           SizedBox(height: 64,),
           Text(
-            question.title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(height: 24,),
-          ...question.answers.map(
-            (e) => AnswerWidget(
-              isRight: e.isRight,
-              title: e.title,
-            ),
-          ).toList(),
-
-            //title: "Kit de desenvolvimento de interface de usuário"
-
-            //title: "Possibilita a criação de aplicativos compilados nativamente",
-           // isRight: true,
-            //
-            //title: "Acho que é uma marca de café do Himalaia"
-            //title: "Possibilita a criação de desktops que são muito incríveis"
+          for(var i = 0; i < widget.question.answers.length; i++)
+          AnswerWidget(
+              answer: answer(i),
+              isSelected: indexSelected == i,
+              onTap: (){
+                indexSelected = i;
+                setState(() {});
+              },
+          ),
         ],
       ),
     );
