fec44ee1f216be17e1c31805f2c6a5f956ca2116
Sheela Guilhermino Khusal <sheela.khusal.08@gmail.com>
2021-04-25 23:27:25 -0300

    Added share_plus and share function on compartilhar button

--- lib/result/result_page.dart
+++ lib/result/result_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:devquiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:devquiz/core/app_images.dart';
 import 'package:devquiz/core/app_text_styles.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ResultPage extends StatelessWidget {
   final String title;
@@ -68,7 +69,9 @@ class ResultPage extends StatelessWidget {
                     child: NextButtonWidget.purple(
                       label: "Compartilhar",
                       onTap: () {
-
+                        Share.share(
+                          'DevQuiz NLW 5 - Flutter: Resultado do Quiz: $title\nObtive ${result / length * 100}% aproveitamento!'
+                        );
                       }),
                   ),
                 ),
